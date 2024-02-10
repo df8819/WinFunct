a3a90e14d835257005cb89dcaa9e5263b1102313
df8819 <df1.00@outlook.com>
2024-02-10 02:00:03 +0100

    Update WinFunct.py

--- WinFunct.py
+++ WinFunct.py
@@ -16,7 +16,7 @@ from JChatInt import JChat
 from SimplePWGenInt import SimplePWGen
 
 # Version of the app
-VERSION = "Use at your own risk and responsibility - v1.2.1.0"
+VERSION = "Use at your own risk and responsibility - v1.2.2.0"
 
 # GitHub repo link
 LINK = "https://github.com/df8819/WinFunct"
