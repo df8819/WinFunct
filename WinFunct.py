b3f480c1b108a915a2b6c95dd1f0feb5f63235b1
df8819 <df1.00@outlook.com>
2024-05-08 13:59:00 +0200

    Update WinFunct.py

--- WinFunct.py
+++ WinFunct.py
@@ -18,7 +18,7 @@ from HashStuffInt import HashStuff
 
 
 # Version of the app
-VERSION = "Use at your own risk and responsibility - v1.3.1.0"
+VERSION = "Use at your own risk and responsibility - v1.3.3.0"
 
 # GitHub repo link
 LINK = "https://github.com/df8819/WinFunct"
