f75a378848794a3e4e7c78800992a58a9d9bf3f8
df8819 <df1.00@outlook.com>
2024-08-30 15:57:53 +0200

    Update WinFunct.py

--- WinFunct.py
+++ WinFunct.py
@@ -2192,7 +2192,7 @@ if !status_code! equ 200 (
         options_notebook.add(netsh_frame, text='Shell Commands')
 
         # Packing the notebook into the options_frame
-        options_notebook.pack(fill='both', expand=True, padx=20, pady=20)
+        options_notebook.pack(fill='both', expand=True, padx=25, pady=10)
 
         # Option Buttons for Option Tab (UI element)
         # Windows Management and Configuration Tools
