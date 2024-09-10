700219d0d6af26fcfba7903d852e082127dc4898
df8819 <df1.00@outlook.com>
2024-09-10 11:36:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ LOGO = f"""
 """
 
 # WinFunct version number
-VERSION_NUMBER = "1.663"
+VERSION_NUMBER = "1.664"
 
 # Use the version number in different strings
 VERSION = f"Use at your own risk and responsibility - v{VERSION_NUMBER}"
