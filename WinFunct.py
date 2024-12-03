6c8fcd92856577f691f406cbe975cc7de029033e
df8819 <74317319+df8819@users.noreply.github.com>
2024-12-03 16:09:15 +0100

    Update WinFunct.py

--- WinFunct.py
+++ WinFunct.py
@@ -185,7 +185,8 @@ def log_message(message):
 def run_as_admin():
     if sys.platform == "win32":
         cmd = [sys.executable] + sys.argv
-        cmd_line = ' '.join(f"\"{item.replace('\"', '\\\"')}\"" for item in cmd)
+        escaped_cmd = [item.replace('"', '\\"') for item in cmd]
+        cmd_line = ' '.join(f'"{item}"' for item in escaped_cmd)
         try:
             log_message("Initial checks completed. Running as intended...")
             ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, cmd_line, None, 1)
