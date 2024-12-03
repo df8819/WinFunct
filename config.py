bf1da969719aca259843b29d1fdd6ca6e1d587ae
df8819 <74317319+df8819@users.noreply.github.com>
2024-12-03 15:53:19 +0100

    disk checker fix

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ LOGO = f"""
 """
 
 # WinFunct version number
-VERSION_NUMBER = "1.686"
+VERSION_NUMBER = "1.687"
 
 # Use the version number in different strings
 VERSION = f"Use at your own risk and responsibility - v{VERSION_NUMBER}"
